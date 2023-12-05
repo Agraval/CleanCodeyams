3b4d9c93316818cc9471bb483ee7055df4fbc3e7
agraval <alexandre.samson78@gmail.com>
2023-12-05 15:04:22 +0100

    refracto Brelan

--- yams.js
+++ yams.js
@@ -1,10 +1,13 @@
-function checkBrelan(dice) {
-    const occurrences = dice.reduce((acc, val) => {
+function countDiceValues(dice) {
+    return dice.reduce((acc, val) => {
         acc[val] = (acc[val] || 0) + 1;
         return acc;
     }, {});
-
-    return Object.values(occurrences).some(count => count === 3) ? 28 : 0;
 }
 
-module.exports = { checkBrelan };
+function checkBrelan(dice) {
+    return Object.values(countDiceValues(dice)).some(count => count === 3) ? 28 : 0;
+}
+module.exports = {
+    checkBrelan,
+};
