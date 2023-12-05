40a01d78e66fa89f64a948f0c4e9a500cfef1d6a
agraval <alexandre.samson78@gmail.com>
2023-12-05 15:06:55 +0100

    link testing/Carre

--- yams.js
+++ yams.js
@@ -8,6 +8,10 @@ function countDiceValues(dice) {
 function checkBrelan(dice) {
     return Object.values(countDiceValues(dice)).some(count => count === 3) ? 28 : 0;
 }
+function checkCarre(dice) {
+    return Object.values(countDiceValues(dice)).some(count => count === 4) ? 35 : 0;
+}
 module.exports = {
     checkBrelan,
+    checkCarre,
 };
