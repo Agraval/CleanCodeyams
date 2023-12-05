3474fe583294995490e1e111b599fe45f2a0c17a
agraval <alexandre.samson78@gmail.com>
2023-12-05 15:09:18 +0100

    Ajout tests pour vérifier un Grande Suite

--- yams.test.js
+++ yams.test.js
@@ -2,6 +2,7 @@ const {
     checkBrelan,
     checkCarre,
     checkFull,
+    checkGrandeSuite,
 } = require('./yams');
 
 test('identifie un Brelan', () => {
@@ -16,4 +17,7 @@ test('identifie un Full', () => {
     expect(checkFull([1, 1, 2, 2, 2])).toBe(30);
     expect(checkFull([1, 2, 3, 4, 5])).toBe(0);
 });
-
+test('identifie une Grande Suite', () => {
+    expect(checkGrandeSuite([1, 2, 3, 4, 5])).toBe(40);
+    expect(checkGrandeSuite([1, 1, 1, 2, 3])).toBe(0);
+});
