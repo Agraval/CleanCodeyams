07a2444ec764ec4726b63f279a63f9b573688fb7
agraval <alexandre.samson78@gmail.com>
2023-12-05 14:41:04 +0100

    link testing

--- yams.js
+++ yams.js
@@ -2,5 +2,11 @@ function checkBrelan(dice) {
     const occurrences = countOccurrences(dice);
     return Object.values(occurrences).some(count => count === 3);
 }
+const yams = (rolls) => {
+    const scores = rolls.map(roll => {
+        return getScore(roll);
+    });
+    return scores.reduce((acc, score) => acc + score, 0);
+};
 
-module.exports = { checkBrelan };
+module.exports = { yams, checkBrelan };
