c3ae4339e3ec4070445884e10f9a5df7c1f573aa
agraval <alexandre.samson78@gmail.com>
2023-12-05 14:53:44 +0100

    restart for more tdd

--- yams.test.js
+++ yams.test.js
@@ -1,15 +1,8 @@
 const {
-    yams,
     checkBrelan,
 } = require('./yams');
-
-describe("yams", () => {
-    it("should return 0 for an empty array", () => {
-        const rolls = [];
-        expect(yams(rolls)).toBe(0);
-    });
 
 test('identifie un Brelan', () => {
     expect(checkBrelan([1, 1, 1, 2, 3])).toBe(true);
     expect(checkBrelan([1, 2, 3, 4, 5])).toBe(false);
-});
+})
