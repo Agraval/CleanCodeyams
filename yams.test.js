fe614f34337ca2259bd435e1450d408fa52695d0
agraval <alexandre.samson78@gmail.com>
2023-12-05 15:20:02 +0100

    Ajout tests pour vérifier la figure la plus avantageuse

--- yams.test.js
+++ yams.test.js
@@ -31,3 +31,11 @@ test('calcule la Chance', () => {
     expect(calculateChance([1, 2, 3, 4, 5])).toBe(15);
     expect(calculateChance([6, 6, 6, 6, 6])).toBe(30);
 });
+test('évalue les dés pour la figure la plus avantageuse', () => {
+    expect(evaluateDice([1, 1, 1, 2, 3])).toBe(28); // Brelan
+    expect(evaluateDice([2, 2, 2, 2, 5])).toBe(35); // Carré
+    expect(evaluateDice([1, 1, 2, 2, 2])).toBe(30); // Full
+    expect(evaluateDice([1, 2, 3, 4, 5])).toBe(40); // Grande Suite
+    expect(evaluateDice([4, 4, 4, 4, 4])).toBe(50); // YAMS
+    expect(evaluateDice([1, 2, 3, 4, 6])).toBe(16); // Chance
+});
