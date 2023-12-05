c1365195bab4407c695dc91fa810dc35d5e4c4bf
agraval <alexandre.samson78@gmail.com>
2023-12-05 15:06:04 +0100

    Ajout tests pour vérifier un Carre

--- yams.test.js
+++ yams.test.js
@@ -1,9 +1,14 @@
 const {
     checkBrelan,
+    checkCarre,
 } = require('./yams');
 
 test('identifie un Brelan', () => {
     expect(checkBrelan([1, 1, 1, 2, 3])).toBe(28);
     expect(checkBrelan([1, 2, 3, 4, 5])).toBe(0);
+});
+test('identifie un Carré', () => {
+    expect(checkCarre([2, 2, 2, 2, 5])).toBe(35);
+    expect(checkCarre([1, 2, 3, 4, 5])).toBe(0);
 });
 
