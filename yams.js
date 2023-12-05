edcf2f575a77346af0799cd7ba4186b382bd2c8c
agraval <alexandre.samson78@gmail.com>
2023-12-05 14:38:56 +0100

    new testing

--- yams.js
+++ yams.js
@@ -1,4 +1,6 @@
 function checkBrelan(dice) {
+    const occurrences = countOccurrences(dice);
+    return Object.values(occurrences).some(count => count === 3);
 }
 
 module.exports = { checkBrelan };
